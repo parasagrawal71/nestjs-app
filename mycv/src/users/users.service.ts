0a81a26375be2555c55a62b816f5d320ab9b3c20
Paras Agrawal <parasagrawal71@gmail.com>
2024-03-21 12:00:13 +0000

    Add createUser route

--- mycv/src/users/users.service.ts
+++ mycv/src/users/users.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  //   repo: Repository<User>;
+  //   constructor(repo: Repository<User>) {
+  //     this.repo = repo;
+  //   }
+  //     ||
+  //     \/
+  // constructor(private repo: Repository<User>) {}
+  constructor(@InjectRepository(User) private repo: Repository<User>) {} // IMPORTANT: Because of '@InjectRepository(User)' we don't need to create repository explicitly
+
+  create(email: string, password: string) {
+    const user = this.repo.create({
+      email,
+      password,
+    });
+
+    return this.repo.save(user);
+  }
+}
