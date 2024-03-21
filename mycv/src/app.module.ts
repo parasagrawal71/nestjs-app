fe46089fd6c98dc830a0b93734e4751e148c0184
Paras Agrawal <parasagrawal71@gmail.com>
2024-03-21 11:26:03 +0000

    Add report entity

--- mycv/src/app.module.ts
+++ mycv/src/app.module.ts
@@ -5,14 +5,15 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [User], // STEP 1_3: Link the entity to the app module
-      synchronize: true,
+      entities: [User, Report], // STEP 1_3: Link the entity to the app module
+      synchronize: true, // IMPORTANT: Only for development environment. Automatically syncs SQL tables(for example, creates/deletes columns) based on entity files.
     }),
     UsersModule,
     ReportsModule,
