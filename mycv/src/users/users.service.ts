efa777f559e08f1cb255689033bacdbc6903eecc
Paras Agrawal <parasagrawal71@gmail.com>
2024-03-28 10:15:22 +0000

    Add user service methods

--- mycv/src/users/users.service.ts
+++ mycv/src/users/users.service.ts
@@ -18,11 +18,40 @@ export class UsersService {
     const user = this.repo.create({
       email,
       password,
-    });
+    }); // NOTE: Returns a user entity instance
 
     return this.repo.save(user);
 
     // return this.repo.save({ email, password });
     // NOTE: This also works but the upper one is recommended for validation at entity level and hooks such as AfterInsert are not executed in this case
+  }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    // return this.repo.update({ id }, attrs); // NOTE: It works but without excuting hooks
+
+    const user = await this.findOne(id);
+    if (!user) {
+      return new Error('User not found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    // return this.repo.delete(id); // NOTE: It works but without excuting hooks
+
+    const user = await this.findOne(id);
+    if (!user) {
+      return new Error('User not found');
+    }
+    return this.repo.remove(user);
   }
 }
