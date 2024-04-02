f764615058d0cc58f1a25b83608df9a275ed3d81
Paras Agrawal <parasagrawal71@gmail.com>
2024-04-02 13:13:10 +0000

    Setup ormconfig for db connection

---
+++ mycv/ormconfig.js
@@ -0,0 +1,26 @@
+const dbConfig = {
+  synchronize: false,
+};
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: ['**/*.entity.js'],
+    });
+    break;
+  case 'test':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'test-db.sqlite',
+      entities: ['**/*.entity.ts'],
+    });
+    break;
+  case 'production':
+    break;
+  default:
+    throw new Error('Unknown Environment');
+}
+
+module.exports = dbConfig;
