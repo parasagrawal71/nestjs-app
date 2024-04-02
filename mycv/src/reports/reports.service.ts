a834e54a404e67c4ea5b6cca4ff48815b0ee44d9
Paras Agrawal <parasagrawal71@gmail.com>
2024-04-02 06:36:56 +0000

    Modify get estimate query

--- mycv/src/reports/reports.service.ts
+++ mycv/src/reports/reports.service.ts
@@ -43,6 +43,7 @@ export class ReportsService {
         .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
         .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
         .andWhere('year - :year BETWEEN -3 AND 3', { year })
+        .andWhere('approved IS TRUE')
         // .orderBy('mileage - :mileage', { mileage }) // IMPORTANT: orderBy doesn't take a second argument
         .orderBy('ABS(mileage - :mileage)', 'DESC')
         .setParameters({ mileage })
